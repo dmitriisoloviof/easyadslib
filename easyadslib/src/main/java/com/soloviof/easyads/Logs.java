0c83d8c948d530d99ed262c13ac71dc720b06acb
dmitriisoloviof <dmitrii.soloviof@gmail.com>
2018-10-25 12:00:31 +0300

    v1.43

--- easyadslib/src/main/java/com/soloviof/easyads/Logs.java
+++ easyadslib/src/main/java/com/soloviof/easyads/Logs.java
@@ -4,8 +4,9 @@ import android.util.Log;
 
 class Logs {
 
-     static void print(String theClass, String theMethod, String theComment) {
-        Log.d("Ads ==>", "cls: " + theClass + " mth : " + theMethod + " cm : " + theComment);
+    static void print(String theClass, String theMethod, String theComment) {
+        if (BuildConfig.DEBUG)
+            Log.d("Ads ==>", "cls: " + theClass + " mth : " + theMethod + " cm : " + theComment);
     }
 
 }
