c24fbd50f657d1ac1796759e537f29d83b31928e
dmitriisoloviof <dmitrii.soloviof@gmail.com>
2018-10-26 10:17:08 +0300

    v1.51

--- easyadslib/src/main/java/com/soloviof/easyads/InterstitialAddsHelper.java
+++ easyadslib/src/main/java/com/soloviof/easyads/InterstitialAddsHelper.java
@@ -5,17 +5,16 @@ import android.content.Context;
 import com.google.android.gms.ads.InterstitialAd;
 
 public class InterstitialAddsHelper {
-
-    private static InterstitialAd mInterstitialAd;
+    public static InterstitialAd mInterstitialAd;
 
     public static void prepareInterstitialAds(Context context, int i, String resId) {
         if (mInterstitialAd == null) {
             mInterstitialAd = new InterstitialAd(context);
-            mInterstitialAd.setAdUnitId(AdsRepo.getBannerInterstitial(context,i,resId));
+            mInterstitialAd.setAdUnitId(AdsRepo.getBannerInterstitial(context, i, resId));
         }
 
         if (!mInterstitialAd.isLoaded()) {
-            AdMobListener adMobListener = new AdMobListener(AdsRepo.getBannerInterstitial(context,i,resId), "interstitial", mInterstitialAd);
+            AdMobListener adMobListener = new AdMobListener(AdsRepo.getBannerInterstitial(context, i, resId), "interstitial", mInterstitialAd);
             mInterstitialAd.setAdListener(adMobListener);
             adMobListener.requestNewInterstitial();
         }
