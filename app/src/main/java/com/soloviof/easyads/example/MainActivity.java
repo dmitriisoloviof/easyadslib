5f5b18c75865082750a979979f21ce47ba4ba6f3
dmitriisoloviof <dmitrii.soloviof@gmail.com>
2018-10-29 11:00:28 +0200

    v1.55

--- app/src/main/java/com/soloviof/easyads/example/MainActivity.java
+++ app/src/main/java/com/soloviof/easyads/example/MainActivity.java
@@ -2,8 +2,28 @@ package com.soloviof.easyads.example;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.soloviof.easyads.AdsRepo;
+import com.soloviof.easyads.InitApp;
+import com.soloviof.easyads.TestEncode;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.net.URL;
+import java.security.KeyStore;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
+
+public class MainActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,4 +31,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
     }
+
+
 }
