b3453a483e9e0a1ab62c254542ca59940ed8fe5c
dmitriisoloviof <dmitrii.soloviof@gmail.com>
2019-08-07 11:05:24 +0300

    --- v18.1.1-alpha --- - delete MainActivity testing code

--- app/src/main/java/com/soloviof/easyads/example/MainActivity.java
+++ app/src/main/java/com/soloviof/easyads/example/MainActivity.java
@@ -1,12 +1,7 @@
 package com.soloviof.easyads.example;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-
-import com.soloviof.easyads.AdsRepo;
-import com.soloviof.easyads.InitApp;
-import com.soloviof.easyads.TestEncode;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -20,24 +15,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.btn1:
-                try {
-                    TestEncode.test1(this);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                break;
 
-            case R.id.btn2:
-                String str = AdsRepo.getBannerId1(MainActivity.this, 1, "banner_id_1_app");
-                Log.d("1111", "=> " + str);
-                break;
-
-            case R.id.btn3:
-                InitApp.doFirebaseInit(MainActivity.this, null);
-                break;
-        }
 
     }
 }
